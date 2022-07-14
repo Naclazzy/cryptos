28d0edd7d68748ea19d428616dfa06d585004478
JANWE <jan.wegmann@hotmail.de>
2022-07-14 19:50:26 +0200

    change App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import { Switch, Route, Link} from 'react-router-dom';
+import { Layout, Typography, Space} from 'antd';
+
+import Navbar from './components/navbar'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>Crypto Stats
+    <div className="navbar">
+
+    </div>
+    <div className='main'>
+
+    </div>
+    <div className='footer'>
+
+    </div>
+    </div>
   )
 }
 
